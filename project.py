606fd9861a3bd65935cd0a7514c05994d3655ffb
Changhwan Kim <kimch061279@gmail.com>
2024-10-20 11:32:23 +0900

    edit project.py file; add init_project class

--- project.py
+++ project.py
@@ -3,6 +3,13 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+class init_project(BaseModel):
+    pname: str # 프로젝트 이름
+    pdetails: str # 프로젝트 내용
+    psize: int # 프로젝트 개발 인원
+    pperiod: str # 프로젝트 개발 기간
+    pmm: int #프로젝트 관리 방법론; project management methodologies
+
 @router.post("/project_init")
 async def api_prj_init_post():
     return {}
