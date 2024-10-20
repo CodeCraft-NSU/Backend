9c33227b3c1bf55206da1d6a1139cb591933c608
Changhwan Kim <kimch061279@gmail.com>
2024-10-20 11:42:40 +0900

    edit mysql_connection.py; add test codes

--- mysql_connection.py
+++ mysql_connection.py
@@ -4,9 +4,20 @@ import os
 
 load_dotenv()
 password = os.getenv('DB_PASSWORD')
+
 connection = pymysql.connect(
     host='192.168.50.84',
     user='root',
     password=password,
     charset='utf8mb4'
 )
+
+# try:
+#     with connection.cursor() as cursor:
+#         cursor.execute("SHOW DATABASES")
+#         databases = cursor.fetchall()
+#         print("Databases:")
+#         for db in databases:
+#             print(db[0])
+# finally:
+#     connection.close()
