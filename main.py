569d66334733345b73ddf4e2606ed6a032f0b652
Changhwan Kim <kimch061279@gmail.com>
2024-11-04 15:36:18 +0900

    add comments

--- main.py
+++ main.py
@@ -5,17 +5,22 @@
    생성자   : 김창환                                
                                                                               
    생성일   : 2024/10/14                                                       
-   업데이트 : 2024/10/20                                                      
+   업데이트 : 2024/11/4                                                      
                                                                              
    설명     : FastAPI 서버 설정 및 계정, 프로젝트, 업무, 산출물 관리 라우터 포함                   
 """
 
+# 모듈 추가 파트
 from fastapi import FastAPI
+
+# 라우터 추가 파트
 from account import router as account_router
 from project import router as project_router
 from task import router as task_router
 from output import router as output_router
 from test import router as test_router # Frontend Axios에서 API 통신 테스트를 위한 라우터
+
+# Database Project와의 연동을 위해 bashrc에 PYTHONPATH 정의 필요
 
 app = FastAPI()
 
