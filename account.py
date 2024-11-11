c61e588f5b0344a41e428752c5ad95d2b97290fa
Changhwan Kim <kimch061279@gmail.com>
2024-11-11 14:27:20 +0900

    edit account.py; edit functions

--- account.py
+++ account.py
@@ -5,7 +5,7 @@
    생성자   : 김창환                                
                                                                               
    생성일   : 2024/10/16                                                      
-   업데이트 : 2024/10/25                                                      
+   업데이트 : 2024/11/11                                                     
                                                                              
    설명     : 계정 생성, 로그인, 세션 관리를 위한 API 엔드포인트 정의
 """
@@ -32,7 +32,7 @@ class Signin_Payload(BaseModel):
     id: str
     pw: str
 
-def generate_token(user_id: str):
+def generate_token():
     """
     알파벳 대소문자, 숫자, 특수문자를 섞어 15자 길이의 랜덤 토큰 생성
     """
@@ -42,11 +42,11 @@ def generate_token(user_id: str):
     token = ''.join(random.choices(characters, k=15))
     return token
 
-def check_session(user_token: str):
-    """
-    DB와 통신 후 토큰의 유효성을 체크하는 기능 구현
-    """
-    return {}
+def check_session(user_id: str, token: str):
+    # validate_user_token 함수를 호출하여 토큰 유효성 확인
+    if not account_DB.validate_user_token(user_id, token):
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return True
 
 @router.post("/acc/signup")
 async def api_acc_signup_post(payload: SignUp_Payload):
@@ -54,8 +54,8 @@ async def api_acc_signup_post(payload: SignUp_Payload):
     DB에 사용자 정보를 삽입하는 쿼리 실행
     Database Project의 account_DB에 정의돼있는 insert_user 함수 사용
     """
-    Token = generate_token(payload.id) # Session 토큰 생성
-    insert_result = insert_user(payload, Token)  # insert_user 함수는 payload의 정보를 DB에 삽입
+    Token = generate_token() # Session 토큰 생성
+    insert_result = account_DB.insert_user(payload, Token)  # insert_user 함수는 payload의 정보를 DB에 삽입
     if insert_result is True:
         return {"RESULT_CODE": 200,
                 "RESULT_MSG": "Success",
@@ -71,14 +71,14 @@ async def api_acc_signup_post(payload: SignUp_Payload):
 async def api_acc_signin_post(payload: Signin_Payload):
     """
     MySQL과 연동하여 계정 확인 후 로그인 성공 여부 확인
-    예시로, 가상의 함수 validate_user()를 사용한다고 가정
+    Database Project의 account_DB에 정의돼있는 validate_user 함수 사용
     """
     try:
         # 사용자 유효성 검사
-        is_valid_user = validate_user(payload.id, payload.pw)  # validate_user 함수는 ID와 PW를 체크
+        is_valid_user = account_DB.validate_user(payload.id, payload.pw)  # validate_user 함수는 ID와 PW를 체크
         
         if is_valid_user:
-            Token = generate_token(payload.id)  # 'Token' 변수 생성
+            Token = generate_token()  # 'Token' 변수 생성
             return {"RESULT_CODE": 200,
                     "RESULT_MSG": "Success",
                     "PAYLOADS": {
