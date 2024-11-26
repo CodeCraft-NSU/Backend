6a012543b05ce12f37b5d21c27779bf639d07eee
Changhwan Kim <kimch061279@gmail.com>
2024-11-26 10:52:18 +0900

    edit wbs.py; add functions

--- grade.py
+++ grade.py
@@ -1,6 +1,19 @@
+"""                                                          
+   CodeCraft PMS Backend Project                             
+                                                                              
+   파일명   : grade.py                                                          
+   생성자   : 김창환                                
+                                                                              
+   생성일   : 2024/11/24                                                      
+   업데이트 : 2024/11/24                                              
+                                                                             
+   설명     : 프로젝트를 평가와 관련 된 엔드포인트 정의
+"""
+
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+import sys, os
 
 sys.path.append(os.path.abspath('/data/Database Project'))  # Database Project와 연동하기 위해 사용
 import grade_DB
