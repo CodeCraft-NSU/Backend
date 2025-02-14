b4d96b605875389a1ca91c003f7979b78125d7d8
Changhwan Kim <kimch061279@gmail.com>
2025-02-14 12:14:43 +0900

    Edit subject.py; add functions

--- subject.py
+++ subject.py
@@ -18,3 +18,43 @@ sys.path.append(os.path.abspath('/data/Database Project'))
 import subject_DB
 
 router = APIRouter()
+
+class SubjectPayload(BaseModel):
+    dno: int = None
+    univ_id: int = None
+
+@router.post("/subject/load_all")
+async def api_subject_load_all():
+    """등록된 모든 과목을 조회"""
+    try:
+        result = fetch_subject_list()
+        if isinstance(result, Exception):
+            raise HTTPException(status_code=500, detail=f"Error in Load all Operation: {str(result)}")
+        return {"RESULT_CODE": 200, "RESULT_MSG": "Load Successful.", "PAYLOAD": {"Result": result}}
+    except Exception as e:
+        logger.debug(f"Error in Load all Operation: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Unexpected error in Load all Operation: {str(e)}")
+
+@router.post("/subject/load_dept")
+async def api_subject_load_by_dept(payload: SubjectPayload):
+    """특정 학과의 모든 과목을 조회"""
+    try:
+        result = fetch_subject_list_of_dept(payload.dno)
+        if isinstance(result, Exception):
+            raise HTTPException(status_code=500, detail=f"Error in Load dept Operation: {str(result)}")
+        return {"RESULT_CODE": 200, "RESULT_MSG": "Load Successful.", "PAYLOAD": {"Result": result}}
+    except Exception as e:
+        logger.debug(f"Error in Load all Operation: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Unexpected error in Load dept Operation: {str(e)}")
+
+@router.post("/subject/load_student")
+async def api_subject_load_by_student(payload: SubjectPayload):
+    """특정 학생이 속한 학과의 모든 과목을 조회"""
+    try:
+        result = fetch_subject_list_of_student(payload.univ_id)
+        if isinstance(result, Exception):
+            raise HTTPException(status_code=500, detail=f"Error in Load student Operation: {str(result)}")
+        return {"RESULT_CODE": 200, "RESULT_MSG": "Load Successful.", "PAYLOAD": {"Result": result}}
+    except Exception as e:
+        logger.debug(f"Error in Load all Operation: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Unexpected error in Load student Operation: {str(e)}")
