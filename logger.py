a73ee6f1e09aabe75e51a4a81f07f722ad318331
Changhwan Kim <kimch061279@gmail.com>
2025-02-15 15:00:16 +0900

    edit logger.py; add comment

--- logger.py
+++ logger.py
@@ -1,3 +1,15 @@
+"""                                                          
+   CodeCraft PMS Backend Project                             
+                                                                              
+   파일명   : logger.py                                                          
+   생성자   : 김창환                                
+                                                                              
+   생성일   : 2025/02/15
+   업데이트 : 2025/02/15
+                                                                             
+   설명     : 디버깅 관련 로깅 함수 정의
+"""
+
 import logging
 
 logging.basicConfig(
